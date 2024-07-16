6f163e0688d852649eb47aa83d1ba33526104183
Dhia Bouassida <dhiabouassida@gmail.com>
2024-07-16 23:26:48 +0200

    add get_composite_states

--- io_automaton_to_composite.py
+++ io_automaton_to_composite.py
@@ -13,20 +13,24 @@ def get_composite_states(automat) -> list[CompositeState]:
     for message_in, transitions in message_in_map.items():
         if not transitions:
             continue
-        
+
         check = {}
         actions = []
-        if len(transitions) == 1:
-            actions.append(" \n ".join([f"{recipient}.{action}" for (action, recipient, _) in transitions[0].message_out]))
-            composite_state = CompositeState(name=message_in, check=check, actions=actions)
-            composite_states.append(composite_state)
+
+        if len(transitions) == 1 or len(transitions[0].message_out) == 0:
+            if len(transitions) > 1:
+                check[message_in] = [transition.return_v for transition in transitions]
+            for transition in transitions:
+                message_out = transition.message_out[1:] if len(transitions) > 1 else transition.message_out
+                actions.append(" \n ".join([f"{recipient}.{action}" for action, recipient, _ in message_out]))
         else:
-            check[transitions[0].message_out[0][0]] = [transition.message_out[0][2] for transition in transitions] 
+            check[f"{transitions[0].message_out[0][1]}.{transitions[0].message_out[0][0]}"] = [transition.message_out[0][2] for transition in transitions]
             for transition in transitions:
                 message_out = transition.message_out[1:]
-                actions.append(" \n ".join([f"{recipient}.{action}" for (action, recipient, _) in message_out]))
-            
-            composite_state = CompositeState(name=message_in, check=check, actions=actions)
-            composite_states.append(composite_state)
+                actions.append(" \n ".join([f"{recipient}.{action}" for action, recipient, _ in message_out]))
+
+        composite_state = CompositeState(name=message_in, check=check, actions=actions)
+        composite_states.append(composite_state)
+
     return composite_states
     
